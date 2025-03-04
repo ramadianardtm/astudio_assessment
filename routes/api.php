a50cccfe7118e170eea13f63caad848a8f6e39a2
ramadianardtm <ramadianardtm@gmail.com>
2025-03-04 20:09:01 +0700

    Update API

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\API\V1\AttributeController;
 use App\Http\Controllers\API\V1\AuthController;
 use App\Http\Controllers\API\V1\ProjectController;
 use App\Http\Controllers\API\V1\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,19 +25,19 @@ Route::group(['prefix' => 'v1'], function () {
 Route::group(['prefix' => 'v1', 'middleware' => ['verifytoken']], function () {
     //User API
     Route::put('/update-profile', [UserController::class, 'updateProfile']); //Only allow to update our own profile
-    Route::get('/user/{id}', [UserController::class, 'getDetailUser']);
+    Route::get('/user/{user_id}', [UserController::class, 'getDetailUser']);
     Route::get('/users', [UserController::class, 'getAllUsers']);
     Route::delete('/delete-account', [AuthController::class, 'deleteAccount']); //Only allow to delete our own account
 
     //Project API
     Route::get('/projects', [ProjectController::class, 'getProjects']);
-    Route::get('/project/{id}', [ProjectController::class, 'getDetailProject']);
+    Route::get('/project/{project_id}', [ProjectController::class, 'getDetailProject']);
     Route::post('/create-project', [ProjectController::class, 'createProject']);
-    Route::put('/update-project/{id}', [ProjectController::class, 'updateProject']);
-    Route::delete('/delete-project/{id}', [ProjectController::class, 'deleteProject']);
+    Route::put('/update-project/{project_id}', [ProjectController::class, 'updateProject']);
+    Route::delete('/delete-project/{project_id}', [ProjectController::class, 'deleteProject']);
 
-    Route::post('/project/{id}/assign-users', [ProjectController::class, 'assignUsers']);
-    Route::delete('/projects/{projectId}/users/{userId}/unassign', [ProjectController::class, 'unassignUser']);
+    Route::post('/project/{project_id}/assign-users', [ProjectController::class, 'assignUsers']);
+    Route::delete('/projects/{project_id}/users/{user_id}/unassign', [ProjectController::class, 'unassignUser']);
     //Timesheet API
     Route::post('/timesheets/log', [ProjectController::class, 'logTimesheet']);
     Route::get('/timesheets', [ProjectController::class, 'getAllTimesheets']);
@@ -46,10 +45,10 @@ Route::group(['prefix' => 'v1', 'middleware' => ['verifytoken']], function () {
     Route::delete('/delete-timesheet/{timesheet_id}', [ProjectController::class, 'deleteTimesheet']);
     //Attributes API
     Route::get('/attributes', [AttributeController::class, 'getAttributes']);
-    Route::get('/attribute/{id}', [AttributeController::class, 'getDetailAttribute']);
+    Route::get('/attribute/{attribute_id}', [AttributeController::class, 'getDetailAttribute']);
     Route::post('/create-attribute', [AttributeController::class, 'createAttribute']);
-    Route::put('/update-attribute/{id}', [AttributeController::class, 'updateAttribute']);
-    Route::delete('/delete-attribute/{id}', [AttributeController::class, 'deleteAttribute']);
+    Route::put('/update-attribute/{attribute_id}', [AttributeController::class, 'updateAttribute']);
+    Route::delete('/delete-attribute/{attribute_id}', [AttributeController::class, 'deleteAttribute']);
 
     Route::post('/change-password', [AuthController::class, 'changePassword']);
     Route::post('/logout', [AuthController::class, 'logout']);
